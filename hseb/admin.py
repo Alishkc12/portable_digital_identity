a865e0243161b71a8b1c1092da7ea2ead46c4c6e
unknown <kc.alish107@gmail.com>
2021-10-05 21:05:23 +0545

    db solved

--- hseb/admin.py
+++ hseb/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import dept_hseb
+from .models import dept_hseb,student_main,student_marks_eleven,student_marks_twelve
 
 # Register your models here.
-admin.site.register([dept_hseb])
+admin.site.register([dept_hseb,student_main,student_marks_eleven,student_marks_twelve])
--- hseb/models.py
+++ hseb/models.py
@@ -18,3 +18,51 @@ class dept_hseb(models.Model):
     class Meta:
         
         verbose_name_plural = 'hseb_department'    
+
+
+class student_main(models.Model):
+	citizen_student_id = models.CharField(max_length=10)
+	name= models.CharField(max_length=50)
+	eleven_sym=models.IntegerField()
+	twelve_sym=models.IntegerField()
+	img = models.ImageField(upload_to='static/img/dept1/student_hseb')
+
+
+	def __str__(self):
+		return self.citizen_student_id
+
+
+	class Meta:
+		verbose_name_plural='student_main_hseb'	
+
+
+
+class student_marks_eleven(models.Model):
+
+	student=models.ForeignKey(student_main,on_delete=models.CASCADE) 
+	phy= models.FloatField()
+	math= models.FloatField()   
+	chem= models.FloatField()  
+	english= models.FloatField()  
+	nep= models.FloatField()
+	bio= models.FloatField()  
+	comp= models.FloatField()  
+
+	def __str_(self):
+		return self.student  
+
+
+class student_marks_twelve(models.Model):
+
+	student=models.ForeignKey(student_main,on_delete=models.CASCADE) 
+	phy= models.FloatField()
+	math= models.FloatField()   
+	chem= models.FloatField()  
+	english= models.FloatField()  
+	nep= models.FloatField()
+	bio= models.FloatField()  
+	comp= models.FloatField()  
+
+	def __str_(self):
+		return self.student  
+        
